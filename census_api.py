d9c3e794540dae3bd7c6620ec81f8c5a5e2f2909
Jesid Acosta <jesid.acosta@gerdau.com>
2020-07-31 14:24:39 -0400

    checked API is working and how to fetch relevant info

--- census_api.py
+++ census_api.py
@@ -1,6 +1,9 @@
 # %%
 import censusdata
 import pandas as pd
+import sqlite3
+import ssl
+import re
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.precision', 2)
 
@@ -14,7 +17,7 @@ censusdata.geographies(censusdata.censusgeo([('state','12'),('place', '*')]), 'a
 
 # %%
 # By County
-counties = censusdata.geographies(censusdata.censusgeo([('state','12'),('county', '*')]), 'acs5', 2018)
+censusdata.geographies(censusdata.censusgeo([('state','12'),('county', '*')]), 'acs5', 2018)
 
 # %% 
 # By County Subdivision
@@ -26,21 +29,21 @@ censusdata.geographies(censusdata.censusgeo([('state','12'), ('county', '057'), 
 
 
 # %%
-# Example drawing data on ages for Hillsborough County: NOT WORKING with 4 TUPLES
-censusdata.download('acs5', 2018, censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '93367'), ('place/remainder (or part)', '*')]),[('B01001_'+str(x).zfill(3)+'E') for x in list(range(4,50)) if x not in [4,5,6,26,27,28,29,30]])
+# Example drawing data on voting ages(18+) for Hillsborough County/Tampa CCD: 
+tampa = censusdata.download('acs5', 2018, censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '93367'), ('place/remainder (or part)', '*')]),[('B01001_'+str(x).zfill(3)+'E') for x in list(range(4,50)) if x not in [4,5,6,26,27,28,29,30]])
 
 # %%
-# Simplified example
-censusdata.download('acs5', 2018, censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '93367'), ('place/remainder (or part)', '*')]),['B01001_007E'])
+# # Simplified example
+# censusdata.download('acs5', 2018, censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '93367'), ('place/remainder (or part)', '*')]),['B01001_007E'])
 
-# %%
-# Example from documentation: works with three tuples for parameters
-censusdata.download('acs5', 2015,
-                        censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '*')]),
-                        ['B23025_003E', 'B23025_005E', 'B15003_001E', 'B15003_002E', 'B15003_003E',
-                        'B15003_004E', 'B15003_005E', 'B15003_006E', 'B15003_007E', 'B15003_008E',
-                        'B15003_009E', 'B15003_010E', 'B15003_011E', 'B15003_012E', 'B15003_013E',
-                        'B15003_014E', 'B15003_015E', 'B15003_016E'])
+# # %%
+# # Example from documentation: works with three tuples for parameters
+# censusdata.download('acs5', 2015,
+#                         censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '*')]),
+#                         ['B23025_003E', 'B23025_005E', 'B15003_001E', 'B15003_002E', 'B15003_003E',
+#                         'B15003_004E', 'B15003_005E', 'B15003_006E', 'B15003_007E', 'B15003_008E',
+#                         'B15003_009E', 'B15003_010E', 'B15003_011E', 'B15003_012E', 'B15003_013E',
+#                         'B15003_014E', 'B15003_015E', 'B15003_016E'])
 
 # %% [markdown]
 # # Next Steps
@@ -50,5 +53,41 @@ censusdata.download('acs5', 2015,
 #2. Create table with all Geolocations for Florida at the Place or Remainder level
 #3. Create Script for DB storage
 
+# %% [markdown]
+# Exploring dataframe access
+
+# %%
+# checking how to add rows
+tampa['voters'] = tampa.sum(axis=1)
+tampa.head(2)
+
+# %%
+# # checking how to access state, county, subdivision, places values
+counties = censusdata.geographies(censusdata.censusgeo([('state','12'),('county', '*')]), 'acs5', 2018)
+subdivisions = censusdata.geographies(censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '*')]), 'acs5', 2018)
+places = censusdata.geographies(censusdata.censusgeo([('state','12'), ('county', '057'), ('county subdivision', '93367'), ('place/remainder (or part)', '*')]), 'acs5', 2018)
+states = censusdata.geographies(censusdata.censusgeo([('state', '*')]),'acs5', 2018)
+# %% 
+for state in states:
+    print(state)
+    print(states[state].params()[0][1])
+
+# %%
+for county in counties:
+    print(re.findall(r'([A-Za-z.\s]+)\sCounty',county)[0])
+    print(counties[county].params()[1][1])
+
+# %%
+for division in subdivisions:
+    # print(division)
+    print(re.findall(r'([A-Za-z.\s]+)\s..[D],', division)[0])
+    print(subdivisions[division].params()[2][1])
+
+# %%
+for place in places:
+    print(place)
+    # print(re.findall())
+    print(places[place].params())
+    print(places[place].params()[3][1])
 
 # %%
